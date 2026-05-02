a2ae2dcbf8429456f14e3ebc7dd8eb0bd4efecc2
Deztini <agbatordestiny26@gmail.com>
2026-05-02 17:39:51 +0100

    feat: Add Button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,29 @@
+import type { ButtonHTMLAttributes } from "react";
+
+type ButtonSize = "sm" | "lg";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size: ButtonSize;
+  children: React.ReactNode;
+}
+
+const sizeStyles: Record<ButtonSize, string> = {
+  sm: "px-4, py-2 text-sm",
+  lg: "py-8 px-4 text-base",
+};
+
+export function Button({
+  size = "sm",
+  className = "",
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={`inline-flex items-center justify-center font-poppins font-semibold rounded-sm transition-all duration-200 cursor-pointer bg-[#DB3507] text-[#F5F5F5] hover:bg-[#cf4e14] active:scale-95 shadow-md hover:shadow-orange-500/30 hover:shadow-lg ${sizeStyles[size]} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
